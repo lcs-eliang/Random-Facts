4b3f3ff6165f93af4914bbe13d965aa205cd88d3
Emily Liang <eliang@lcs.on.ca>
2020-11-30 12:27:40 -0500

    Decided to do Random Facts as my final app of the year; added the codes from Pow WOW and tried to change things to do the same things for Random Facts

--- Random Facts/ContentView.swift
+++ Random Facts/ContentView.swift
@@ -27,7 +27,47 @@ struct ContentView: View {
        
     }
     
-    
+    // getting a random fact
+    func fetchrandomfacts() {
+        
+        // 1. Prepare a URLRequest to send our encoded data as JSON
+        let url = URL(string: "https://dog.ceo/api/breeds/image/random")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        
+        // 2. Run the request and process the response
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            // handle the result here – attempt to unwrap optional data provided by task
+            guard let doggieData = data else {
+                
+                // Show the error message
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+                
+                return
+            }
+            
+            // It seems to have worked? Let's see what we have
+            print(String(data: doggieData, encoding: .utf8)!)
+            
+            // Now decode from JSON into an array of Swift native data types
+            if let decodedDoggieData = try? JSONDecoder().decode(RandomDog.self, from: doggieData) {
+                
+                print("Doggie data decoded from JSON successfully")
+                print("URL is: \(decodedDoggieData.message)")
+                
+                // Now fetch the image at the address we were given
+                fetchImage(from: decodedDoggieData.message)
+                
+            } else {
+                
+                print("Invalid response from server.")
+            }
+            
+        }.resume()
+        
+    }
     
 }
 
