bdc8fbce9d2fc5f98da32720a0fb669ce41fb747
Emily Liang <eliang@lcs.on.ca>
2020-11-30 11:55:12 -0500

    Initial Commit

---
+++ Random Facts/Random_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_FactsApp.swift
+//  Random Facts
+//
+//  Created by Emily Liang on 2020-11-30.
+//
+
+import SwiftUI
+
+@main
+struct Random_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
